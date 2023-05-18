90aad485aa5825d339d133f9ba7ed1cc6b9967e0
R44VC0RP <89211796+R44VC0RP@users.noreply.github.com>
2023-05-18 11:52:18 -0400

    Update

--- main.py
+++ main.py
@@ -259,5 +259,4 @@ def upload_success(filename):
 
 # This is for heroku
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
